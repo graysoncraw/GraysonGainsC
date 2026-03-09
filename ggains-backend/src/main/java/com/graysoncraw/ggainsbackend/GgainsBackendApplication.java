626a8dd2bc714fd980a6c857060c456e902b824d
graysoncraw <graysoncraw4d@gmail.com>
2026-03-09 18:26:51 -0500

    sb commit

---
+++ ggains-backend/src/main/java/com/graysoncraw/ggainsbackend/GgainsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.graysoncraw.ggainsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgainsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GgainsBackendApplication.class, args);
+    }
+
+}
---
+++ ggains-backend/src/test/java/com/graysoncraw/ggainsbackend/GgainsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graysoncraw.ggainsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgainsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
